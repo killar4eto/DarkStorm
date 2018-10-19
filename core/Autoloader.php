3455ee65c028bf14e31f38128e4a6892d4f20881
Kaloyan Iliev <killar4eto1992@gmail.com>
2018-10-19 14:27:57 +0300

    Fixing licence issue
    Fixing licence issue

--- core/Autoloader.php
+++ core/Autoloader.php
@@ -4,55 +4,16 @@
  *
  * (c) Atanas Harapov <atanas.harapov@abv.bg>
  *
+ * BUILDED THANKS TO PSR-4 standart for Autoloaders
+ * URL: https://github.com/php-fig/fig-standards/
+ *
+ * Thanks to @RaFa, who discover missing credits and urls
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 namespace Core;
 
-/**
- * An example of a general-purpose implementation that includes the optional
- * functionality of allowing multiple base directories for a single namespace
- * prefix.
- * 
- * Given a foo-bar package of classes in the file system at the following
- * paths ...
- * 
- *     /path/to/packages/foo-bar/
- *         src/
- *             Baz.php             # Foo\Bar\Baz
- *             Qux/
- *                 Quux.php        # Foo\Bar\Qux\Quux
- *         tests/
- *             BazTest.php         # Foo\Bar\BazTest
- *             Qux/
- *                 QuuxTest.php    # Foo\Bar\Qux\QuuxTest
- * 
- * ... add the path to the class files for the \Foo\Bar\ namespace prefix
- * as follows:
- * 
- *      <?php
- *      // instantiate the loader
- *      $loader = new \Example\Psr4AutoloaderClass;
- *      
- *      // register the autoloader
- *      $loader->register();
- *      
- *      // register the base directories for the namespace prefix
- *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/src');
- *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/tests');
- * 
- * The following line would cause the autoloader to attempt to load the
- * \Foo\Bar\Qux\Quux class from /path/to/packages/foo-bar/src/Qux/Quux.php:
- * 
- *      <?php
- *      new \Foo\Bar\Qux\Quux;
- * 
- * The following line would cause the autoloader to attempt to load the 
- * \Foo\Bar\Qux\QuuxTest class from /path/to/packages/foo-bar/tests/Qux/QuuxTest.php:
- * 
- *      <?php
- *      new \Foo\Bar\Qux\QuuxTest;
- */
 class Autoloader
 {
     /**
